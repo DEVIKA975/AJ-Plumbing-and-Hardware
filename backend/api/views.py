5e7df3f1716d6707214a646782c91c571fa4e333
DEVIKA975 <devika_aj@outlook.com>
2025-08-11 13:18:55 +0200

    code updated for products and orders

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,6 +5,8 @@ from .serializers import ProductSerializer, OrderSerializer, UserSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.http import HttpResponse
+
 
 @api_view(['GET'])
 def test_api(request):
@@ -18,3 +20,6 @@ class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     permission_classes = [IsAuthenticated]
+
+def home(request):
+    return HttpResponse("Backend is running 🚀")
--- backend/shop_backend/urls.py
+++ backend/shop_backend/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from api import views
 
 urlpatterns = [
+    path('', views.home),  # root URL
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # We will create 'api' app next
+    path('api/', include('api.urls')),
 ]
+
+
+
